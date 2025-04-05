e782adcc28600599decf223541afb23ebca5c0c8
MarlonVieira <marlongabriel_100@hotmail.com>
2025-04-05 14:31:24 -0300

    Teste pastas novas

---
+++ bytebank-herdado/src/Testiii.java
@@ -0,0 +1,20 @@
+
+public class Testiii {
+
+	public static void main(String[] args) {
+		
+		Gerente g1 = new Gerente();
+		
+		g1.setNome("Clide");
+		g1.setSalario(5000);
+
+		Funcionario f = new Funcionario();
+		f.setSalario(2000);
+		
+		ControlBonification controle = new ControlBonification();
+		controle.register(g1);
+		controle.register(f);
+		
+		System.out.println(controle.getSoma());
+	}
+}
