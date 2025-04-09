8c7fb8f0b689aadabe85c1e0a4e7b135c9c733e4
MarlonVieira <marlongabriel_100@hotmail.com>
2025-04-08 21:14:20 -0300

    Adicionar últimas aulas Inteface.

--- bytebank-interface/src/Cliente.java
+++ bytebank-interface/src/Cliente.java
@@ -1,9 +1,19 @@
 
-public class Cliente extends FuncionarioAutenticavel {
+public class Cliente implements Autenticavel {
 
+	private int senha;
+	
 	@Override
-	public double getBonificacao() {
-		return 0;
+	public void setSenha(int senha) {
+		this.senha = senha;
 	}
 
+	@Override
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
